69961949f9b33e9cb8e3e047744719b7ec9df4bc
Nathan <nathancookster@gmail.com>
2014-01-26 20:08:32 -0600

    Add css to update copyright year in case it is out of date, update line-height on header nav, add reference to bootstrap variables.

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,2 +1,10 @@
 //= require modernizr
 //= require_tree .
+
+$(function() {
+	var $yearOnPage = $('#current-year');
+	var currentYear = (new Date()).getFullYear();
+	if($yearOnPage.text() !== currentYear) {
+		$yearOnPage.text(currentYear);
+	}
+});
