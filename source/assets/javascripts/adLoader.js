34340e432d29e0001d17bc3c24f0029fb64ce2fb
Nathan Cook <nathancookster@gmail.com>
2016-01-15 13:17:56 -0800

    remove ad loader test file

--- source/assets/javascripts/adLoader.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-(function(window) {
-  var iframe = document.createElement('iframe');
-
-  // Reset src to the correct value
-  iframe.src = 'http://1.cuzillion.com/bin/resource.cgi?type=js&sleep=5&n=1&t=3529962513130.158';
-
-  document.body.appendChild(iframe);
-})(window);
